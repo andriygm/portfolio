0d5ed5298d826ddb8b31fea4e5a97db24ce97d6e
Andriy Massimilla <andriymassimilla@gmail.com>
2022-09-26 15:48:44 -0400

    Enrich footer design

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,13 +1,25 @@
+import { SocialIcon } from 'react-social-icons';
+
 import styles from '../styles/footer.module.scss'
 
 function Footer() {
     return <>
         <footer className={styles.footer}>
-            <p>Made with ❤️ by Andriy Massimilla</p>
+            <div className={styles.about}>
+                <p>Made with ❤️ by Andriy Massimilla</p>
 
-            <a className='bi bi-github text-decoration-none'
-                href='https://github.com/andriygm/portfolio/'
-                target='_blank' rel='noreferrer'> View this site on GitHub</a>
+                <p>
+                    <a className='bi bi-github text-decoration-none'
+                        href='https://github.com/andriygm/portfolio/'
+                        target='_blank' rel='noreferrer'> View this site on GitHub</a>
+                </p>
+            </div>
+
+            <div className={styles.social}>
+                <SocialIcon url="https://www.linkedin.com/in/andriygm/" bgColor="white" className={styles.icon} target="_blank"></SocialIcon>
+                <SocialIcon url="https://www.github.com/andriygm/" bgColor="white" className={styles.icon} target="_blank"></SocialIcon>
+                <SocialIcon url="https://www.flickr.com/photos/150862593@N05/" bgColor="white" className={styles.icon} target="_blank"></SocialIcon>
+            </div>
         </footer>
     </>
 }
